3c7cfb227f9f1002714a80810b912da3ce1b63c4
Kirill Kornyakov <kirill.kornyakov@gmail.com>
2014-08-20 10:05:12 +0400

    More logging while generating slides

--- slides/gen_slides.sh
+++ slides/gen_slides.sh
@@ -1,5 +1,7 @@
 set -e
 
+echo "Start rendering HTML slides from Markdown"
+
 # Get the Git root
 root="$(git rev-parse --show-toplevel)"
 cd $root/slides
@@ -7,8 +9,10 @@ cd $root/slides
 # Create directory for rendered slides
 rendered_slides_dir="$root/slides-rendered"
 mkdir -p $rendered_slides_dir
+echo "Created directory: $rendered_slides_dir"
 
 # Generating HTML
+echo -e "Processing folders:\n"
 for dir in $(ls -d [0-9]*/);
 do
     echo "dir = $dir"
